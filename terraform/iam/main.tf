91a5526f047a6f79842f4028ab0692d511c4d158
ssdaggupati <150368374+ssdaggupati@users.noreply.github.com>
2025-05-29 01:04:12 -0400

    Modularize IAM role and policy with Terraform

---
+++ terraform/iam/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_role" "github_runner_role" {
+  name = "GitHubSelfHosted-Lambda-Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = "arn:aws:iam::819340487192:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/50B91596F352A8C2877D012F1BC352FB"
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "oidc.eks.us-east-1.amazonaws.com/id/50B91596F352A8C2877D012F1BC352FB:sub" = "system:serviceaccount:arc-runners:github-runner"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_permissions" {
+  name = "LambdaUpdatePolicy"
+  role = aws_iam_role.github_runner_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "lambda:UpdateFunctionCode",
+          "lambda:GetFunction",
+          "sts:GetCallerIdentity"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
---
+++ terraform/iam/output.tf
@@ -0,0 +1,4 @@
+output "iam_role_arn" {
+  value = aws_iam_role.github_runner_role.arn
+}
+
