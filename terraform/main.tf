7da88ba77c38e3ff2d13e7835d3dcd7a445d1215
ssdaggupati <150368374+ssdaggupati@users.noreply.github.com>
2025-05-30 23:35:56 -0400

    Update to use new OIDC Lambda role

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,27 @@
-resource "aws_iam_role" "github_oidc_role" {
-  name = "GitHubSelfHosted-Lambda-Role"
+resource "aws_iam_role" "github_lambda_oidc_role" {
+  name = "GitHubSelfHosted-Lambda-Role-v2"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Federated = "arn:aws:iam::819340487192:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/46E5864AB37FD92F0339A9D7B7EDD59C"
-      },
-      Action = "sts:AssumeRoleWithWebIdentity",
-      Condition = {
-        StringEquals = {
-          "oidc.eks.us-east-1.amazonaws.com/id/46E5864AB37FD92F0339A9D7B7EDD59C:sub" = "system:serviceaccount:arc-runners:github-runner"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = "arn:aws:iam::819340487192:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/46E5864AB37FD92F0339A9D7B7EDD59C"
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "oidc.eks.us-east-1.amazonaws.com/id/46E5864AB37FD92F0339A9D7B7EDD59C:sub" = "system:serviceaccount:arc-runners:github-runner"
+          }
         }
       }
-    }]
+    ]
   })
 }
 
-resource "aws_iam_policy" "lambda_custom_policy" {
-  name        = "GitHubLambdaDeployPolicy"
-  description = "Minimum permissions to deploy and manage Lambda functions"
+resource "aws_iam_policy" "github_lambda_deploy_policy" {
+  name = "GitHubLambdaDeployPolicy-v2"
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -32,13 +33,22 @@ resource "aws_iam_policy" "lambda_custom_policy" {
           "lambda:UpdateFunctionCode",
           "lambda:UpdateFunctionConfiguration"
         ],
+        Resource = "arn:aws:lambda:us-east-1:819340487192:function:test_lambda"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
         Resource = "*"
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_custom_policy_attach" {
-  policy_arn = aws_iam_policy.lambda_custom_policy.arn
-  role       = aws_iam_role.github_oidc_role.name
+resource "aws_iam_role_policy_attachment" "github_lambda_deploy_policy_attach" {
+  policy_arn = aws_iam_policy.github_lambda_deploy_policy.arn
+  role       = aws_iam_role.github_lambda_oidc_role.name
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,3 @@
 output "iam_role_arn" {
-  description = "IAM Role ARN for GitHub self-hosted runner to deploy Lambda"
-  value       = aws_iam_role.github_oidc_role.arn
+  value = aws_iam_role.github_lambda_oidc_role.arn
 }
